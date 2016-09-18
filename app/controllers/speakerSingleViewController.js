c5829ec65f10b94686955f556df3bd1dee686b22
Paul Lichtenberger <paul.lichtenberger@gmx.de>
2016-09-18 22:34:59 +0200

    add autocomplete single speaker view

--- app/controllers/speakerSingleViewController.js
+++ app/controllers/speakerSingleViewController.js
@@ -112,7 +112,7 @@ seriesAnalyzer.controller('singleSpeakerController', ['$scope', '$http', 'Curren
                 $scope.speakers = data;
                 var dropdownSpeakerNames = [];
                 for (var i = 0; i < $scope.speakers.length; i++) {
-                    console.log($scope.speakers[i]);
+                    console.log($scope.speakers[i].name);
                     dropdownSpeakerNames.push(String.valueOf($scope.speakers[i].name));
                 }
                 $scope.dropdownSpeakerNames = dropdownSpeakerNames;
